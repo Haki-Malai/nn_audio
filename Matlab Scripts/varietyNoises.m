ebcf69baf3b281da9eb0f9a3760e638006eba6b8
Haki-Malai <p17mala@ionio.gr>
2021-05-04 22:19:26 +0300

    Updating Progress

--- Matlab Scripts/varietyNoises.m
+++ Matlab Scripts/varietyNoises.m
@@ -8,10 +8,10 @@ for i=1:length(randomTracks)
     try
         [y, fs]=audioread (strcat("..\\dataset\\wav\\True\\", path(randomTracks(1,i)).name));
         y=imnoise(y, 'gaussian', 0.01, 0.025);
-        audiowrite(strcat("..\\dataset\\wav\\False\\", 'SNR11Noise', int2str(i), '.wav'), y, 44100);
+        audiowrite(strcat('GaussianNoise', int2str(i), '.wav'), y, 44100);
     catch
-        strcat("Couldn't write song ", erase(path(randomTracks(1,i)).name, '.wav'))
-        errors = errors + 1
+       strcat("Couldn't write song ", erase(path(randomTracks(1,i)).name, '.wav'))
+    errors = errors + 1,
     end
     i
 end
@@ -23,7 +23,7 @@ for i=1:length(randomTracks)
         Mean=0.00;
         Var=0.005;
         y=imnoise(y,'poisson');
-        audiowrite(strcat("..\\dataset\\wav\\False\\", 'SNR11Noise', int2str(i), '.wav'), y, 44100);
+        audiowrite(strcat('PoissonNoise', int2str(i), '.wav'), y, 44100);
     catch
         strcat("Couldn't write song ", erase(path(randomTracks(1,i)).name, '.wav'))
         errors = errors + 1
@@ -38,7 +38,7 @@ for i=1:length(randomTracks)
         Mean=0.00;
         Var=0.005;
         y=imnoise(y,'salt & pepper', 0.025);
-        audiowrite(strcat("..\\dataset\\wav\\False\\", 'SNR11Noise', int2str(i), '.wav'), y, 44100);
+        audiowrite(strcat('SaltNPepperNoise', int2str(i), '.wav'), y, 44100);
     catch
         strcat("Couldn't write song ", erase(path(randomTracks(1,i)).name, '.wav'))
         errors = errors + 1
@@ -53,7 +53,7 @@ for i=1:length(randomTracks)
         Mean=0.00;
         Var=0.005;
         y=imnoise(y,'salt & pepper', 0.005);
-        audiowrite(strcat("..\\dataset\\wav\\False\\", 'SNR11Noise', int2str(i), '.wav'), y, 44100);
+        audiowrite(strcat('SaltNPepper0005Noise', int2str(i), '.wav'), y, 44100);
     catch
         strcat("Couldn't write song ", erase(path(randomTracks(1,i)).name, '.wav'))
         errors = errors + 1
@@ -67,7 +67,7 @@ for i=1:length(randomTracks)
         Mean=0.00;
         Var=0.005;
         y=imnoise(y,'speckle');
-        audiowrite(strcat("..\\dataset\\wav\\False\\", 'SNR11Noise', int2str(i), '.wav'), y, 44100);
+        audiowrite(strcat('SpeckleNoise', int2str(i), '.wav'), y, 44100);
     catch
         strcat("Couldn't write song ", erase(path(randomTracks(1,i)).name, '.wav'))
         errors = errors + 1
@@ -82,7 +82,7 @@ for i=1:length(randomTracks)
         Mean=0.00;
         Var=0.005;
         y=imnoise(y,'speckle', 0.25);
-        audiowrite(strcat("..\\dataset\\wav\\False\\", 'SNR11Noise', int2str(i), '.wav'), y, 44100);
+        audiowrite(strcat('SpeckleNoise', int2str(i), '.wav'), y, 44100);
     catch
         strcat("Couldn't write song ", erase(path(randomTracks(1,i)).name, '.wav'))
         errors = errors + 1
@@ -97,7 +97,7 @@ for i=1:length(randomTracks)
         Mean=0.00;
         Var=0.005;
         y = awgn(y,11,'measured');
-        audiowrite(strcat("..\\dataset\\wav\\False\\", 'SNR11Noise', int2str(i), '.wav'), y, 44100);
+        audiowrite(strcat('Measured11Noise', int2str(i), '.wav'), y, 44100);
     catch
         strcat("Couldn't write song ", erase(path(randomTracks(1,i)).name, '.wav'))
         errors = errors + 1
@@ -112,7 +112,7 @@ for i=1:length(randomTracks)
         Mean=0.00;
         Var=0.005;
         y = awgn(y,9,'measured');
-        audiowrite(strcat("..\\dataset\\wav\\False\\", 'SNR11Noise', int2str(i), '.wav'), y, 44100);
+        audiowrite(strcat('Measured9Noise', int2str(i), '.wav'), y, 44100);
     catch
         strcat("Couldn't write song ", erase(path(randomTracks(1,i)).name, '.wav'))
         errors = errors + 1
